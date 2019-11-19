3a461f66eade01c3554a2b9cefae830d933f73db
Michele_Manco <motlamik@gmail.com>
2019-11-19 19:42:39 +0100

    controls added

--- main.js
+++ main.js
@@ -4,13 +4,20 @@ console.log(numerocas);
 var insert = parseInt(prompt('inserisci un numero intero fra 1 e 6'));
 console.log(insert);
 
-if (numerocas > insert) {
-  console.log('hai perso');
+if (isNaN(insert && insert < 1 && insert > 6)) {
+  console.log('il numero inserito non è corretto');
+} else {
+  console.log('il numero digitato è ' + insert);
 
-} else if (insert > numerocas) {
-  console.log('hai vinto');
 
-}
-else {
-  console.log('pari');
+  if (numerocas > insert) {
+    console.log('hai perso');
+
+  } else if (insert > numerocas) {
+    console.log('hai vinto');
+
+  }
+  else {
+    console.log('pari');
+  }
 }
